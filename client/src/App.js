967e94fbe330eb24c41fc05a7283ddfae51a3d0a
Lewis-Eason <easonlewis00@gmail.com>
2022-06-03 22:06:45 +0100

    Created layout for login page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import $ from 'jquery';
 import Axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
+import {Form, Button, Row, Col, Container} from "react-bootstrap"
 
 function App() {
 
@@ -19,20 +22,30 @@ function App() {
     }
 
   return (
-    <div className="App">
-            <div className="container">
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username"/>
-                <label htmlFor="password">Password:</label>
-                <input type="text" id="password" name="password"/>
-                <div className="error">Invalid login</div>
-                <span>
-                    <a href="#">Forgot your password?</a>
-                    <p>Not a member? <a href="#">Click here</a> to create an account.</p>
-                </span>
-                <input type="submit" value="Login" className="button-green" onClick={login}/>
-            </div>
-    </div>
+      <>
+          <Container>
+              <h1 className="shadow-sm text-success mt-5 p-3 text-center rounded">Login</h1>
+              <Row className="mt-5">
+                  <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
+                      <Form>
+                          <Form.Group controlId="formBasicEmail">
+                              <Form.Label>Username</Form.Label>
+                              <Form.Control type="text" placeholder="Username" id="username" name="username"/>
+                          </Form.Group>
+
+                          <Form.Group controlId="formBasicPassword" className="text-section-break">
+                              <Form.Label>Password</Form.Label>
+                              <Form.Control type="password" placeholder="Password" id="password" name="password"/>
+                          </Form.Group>
+
+                          <Button variant="success btn-block" type="submit" className="text-section-break" onClick={login}>Login</Button>
+                          <p className="text-section-break"><a href="#">Forgot your password?</a></p>
+                          <p className="text-section-break">Not a member? <a href="#">Click here</a> to create an account.</p>
+                      </Form>
+                  </Col>
+              </Row>
+          </Container>
+      </>
   );
 }
 
